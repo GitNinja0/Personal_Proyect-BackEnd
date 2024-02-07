ce66982d8bdde095c714b6c08b3bbfd59e14df59
Juan Munguia <carlosmunguia23145@gmail.com>
2024-02-07 08:35:52 +0100

    inicializated repo

---
+++ src/main/java/com/personalproyect/personal_proyect_backend/PersonalProyectBackendApplication.java
@@ -0,0 +1,13 @@
+package com.personalproyect.personal_proyect_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalProyectBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalProyectBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/personalproyect/personal_proyect_backend/PersonalProyectBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.personalproyect.personal_proyect_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalProyectBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
